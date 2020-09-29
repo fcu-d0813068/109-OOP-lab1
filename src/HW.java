b48a6776bec999fa8208a2667789bddc4de7a951
user <user@EC08>
2020-09-29 09:35:53 +0800

    1

---
+++ src/HW.java
@@ -0,0 +1,9 @@
+
+public class HW {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("I'm ¸â³Õ³ì");
+			System.out.println("My nid is D0813068");
+			System.out.println("This is lab 1 hw");
+	}
+}
